e2103634660cef43ec9f58fc779468846fd447a5
pzzy2000 <pzzy2000@126.com>
2020-06-16 02:27:46 +0800

    i

---
+++ cloudstore-admin/src/api/supplier.js
@@ -0,0 +1,11 @@
+import request from '@/utils/request'
+import {switchForm} from '@/api/iunits'
+
+export function    searchSupplierDetail(supplierId){
+  console.log(supplierId)
+    return request({
+      url:'/sys/supplier/searchdetail',
+      method:'get',
+      params:supplierId
+    })
+}
