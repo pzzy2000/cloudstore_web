92134f38ce43014fc810876b44a5b6abc9e023e1
雨泉十代 <1400505139@qq.com>
2020-06-23 13:30:15 +0800

    添加了分类页面功能和购物侧页面，附近商家页面

---
+++ cloudstore_uniapp_agent/pages/nearbyBusiness/nearbyBusiness.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<text>这是附近商品</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
