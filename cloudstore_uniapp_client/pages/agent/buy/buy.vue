dbe829b7c90a408cea6d2c5d6a55db8bfcb2138c
雨泉十代 <1400505139@qq.com>
2020-07-03 09:43:29 +0800

    整合添加了agent中的一些文件

---
+++ cloudstore_uniapp_client/pages/agent/buy/buy.vue
@@ -0,0 +1,322 @@
+<template>
+	<view>
+		<nav-bar backState="1000">支付</nav-bar>
+		<view class="goods-list">
+			<view class="goods-item" @click="navToDetailPage(item)">
+				<view class="image-wrapper">
+					<image :src="goodsDetail.goodsPhotos[0].url" mode="aspectFill"></image>
+				</view>
+				<view class="goods-detail">
+					<text class="title clamp">{{ goodsDetail.goodsName}}</text>
+					<text class="title clamp subhead ">{{goodsDetail.goodsSubtitle}}</text>
+					<view class="price-box">
+						<view class="price">
+							<text class="priceSale">{{price}}</text>
+						</view>
+					</view>
+				</view>
+			</view>
+		</view>
+		<view class="num">
+			<text>数量</text>
+			<button @click="subtractNum">-</button>
+			<text>{{num}}</text>
+			<button @click="addNum()">+</button>
+		</view>
+		<view>
+			<text>共计：</text>
+			{{totalPrice}}
+		</view>
+		<view class="uni-form-item uni-column">
+			<view class="title">支付方式</view>
+			<view class="uni-list">
+				<radio-group @change="radioChange" class="buy-type">
+					<label class="uni-list-cell uni-list-cell-pd" v-for="(item, index) in items" :key="item.value">
+						<view>
+							<radio :value="item.value" :checked="index === current" />
+						</view>
+						<view>{{item.name}}</view>
+					</label>
+				</radio-group>
+			</view>
+		</view>
+		<button type="primary" @click="buy">支付</button>
+		<p></p>
+		
+	</view>
+</template>
+	</view>
+<script>
+	import navBar from '@/components/zhouWei-navBar';
+    import Api from '@/common/api.js';
+	export default {
+		data() {
+			return {
+				goodsId: '',
+				goodsDetail: '',
+				price: '',
+				num: '1',
+				totalPrice: '',
+				buyInfo: '',
+				buyType: '',
+				items: [{
+						value: 'USA',
+						name: '支付宝'
+					},
+					{
+						value: 'CHN',
+						name: '微信',
+						checked: 'true'
+					}
+				],
+				current: 0,
+				vxCode: ''
+			}
+		},
+		onLoad(option) {
+			//这里的数据用来做测试
+			// option.goodsId = "7604921082513985536"
+			// option.price = '100'
+			var that = this;
+			this.price = option.price
+			this.totalPrice = option.price
+			this.getGoodsData(option.goodsId)
+		},
+		components: {
+			navBar
+		},
+		methods: {
+			async getGoodsData (goodsId) { //加载商品数据
+				let params = { 
+					goodsId: goodsId ,
+				};
+				let data = await Api.apiCall('post', Api.goods.detail, params, false, false);
+				if (data) {
+					this.goodsDetail = data.result.goodsPicesBean
+				}
+			},
+			radioChange: function(evt) { //选择支付方式
+				for (let i = 0; i < this.items.length; i++) {
+					if (this.items[i].value === evt.target.value) {
+						this.current = i;
+						break;
+					}
+				}
+			},
+			computNumber (price) {
+				return Number(this.price) * 100
+			},
+			subtractNum() { //商品数量的简单减少
+				if (this.num >1) {
+					this.num = parseInt(this.num) - 1
+					this.totalPrice =  Number(parseInt(this.num) * this.computNumber(this.price))/100
+				}
+			},
+			addNum() { //商品数量的简单增加
+				this.num = parseInt(this.num) + 1
+				this.totalPrice =  Number(parseInt(this.num) * this.computNumber(this.price))/100
+			},
+			buy () { //点击支付按钮
+				var that = this;
+				this.buyInfo = uni.getStorageSync('buyInfo')
+				console.log(this.buyInfo)
+				uni.login({
+				    provider: 'weixin',
+				    success: function (loginRes) {
+					   let  vxCode = loginRes.code
+					   console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  "+vxCode);
+					   uni.getProvider({ //获取支付的方式
+					       service: 'payment',
+					       success: function (res) {
+					   		that.buyType = res.provider
+					   		let params = {
+					   			'bean.activityId': that.buyInfo.activityId,
+					   			'bean.agentGoodsId': that.buyInfo.agentGoodsId,
+					   			'bean.goodsId': that.buyInfo.goodsId,
+					   			'bean.goodsSkuId': that.buyInfo.goodsSkuId,
+					   			'bean.number': that.num,
+					   			'bean.payPrice': that.totalPrice,
+					   			'bean.price': that.price,
+					   			'bean.payType': 'weixin'
+					   		}
+					   		uni.request({
+					   			url: Api.BASEURI + Api.buy.createOrder,
+					   			method: 'post',
+					   			header: {
+					   				'content-type': 'application/x-www-form-urlencoded',
+					   				'auth': uni.getStorageSync('token')
+					   			},
+					   			data: params,
+					   			success: res => {
+					   				var orderId = res.data.result.result.id
+					   				if (res.data.result.code === 0) {
+					   					let params = {
+					   						'bean.code': vxCode,
+					   						'bean.orderId': orderId
+					   					}
+					   					console.log(params)
+					   					//预支付
+					   					uni.request({
+					   						url: Api.BASEURI + Api.buy.prePay,
+					   						method: 'post',
+					   						header: {
+					   							'content-type': 'application/x-www-form-urlencoded',
+					   							'auth': uni.getStorageSync('token')
+					   						},
+					   						data: params,
+					   						success: res => {
+					   							console.log(res)
+												if (res) {
+													that.payMent(res)
+												}
+					   						},
+					   						fail: (e) => {
+					   							uni.showToast({
+					   								title: '调用微信预支付失败',
+					   								icon: 'none'
+					   							});
+					   						}
+					   					});
+					   				}
+					   			},
+					   			fail: (e) => {
+					   				uni.showToast({
+					   					title: '创建订单失败',
+					   					icon: 'none'
+					   				});
+					   			}
+					   		});
+					       }
+					   });
+					   
+					   
+				    }
+				});
+			},
+			payMent(res) {
+				let vxBuyInfo = res.data.result.result
+				//微信支付
+				uni.requestPayment({
+					provider: 'wxpay',
+					//orderInfo: orderInfo, //订单数据
+					timeStamp: vxBuyInfo.timeStamp, //时间戳从1970年1月1日至今的秒数，即当前的时间。
+					nonceStr: vxBuyInfo.nonceStr, //随机字符串，长度为32个字符以下。
+					package: vxBuyInfo.packageValue, //统一下单接口返回的 prepay_id 参数值，提交格式如：prepay_id=xx。
+					signType: vxBuyInfo.signType, //签名算法，暂支持 MD5。
+					paySign: vxBuyInfo.paySign, //签名，具体签名方案参见 微信小程序支付文档
+					success: function(res) {
+						console.log(res)
+					},
+					fail: function(err) {
+						console.log('fail:' + JSON.stringify(err));
+						uni.showToast({
+							icon: 'none',
+							title: '支付失败'
+						});
+					},
+					complete: function(res) {
+						
+					}
+				});
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+/* 商品列表 */
+.goods-list {
+	display: flex;
+	flex-wrap: wrap;
+	padding: 10upx 30upx;
+	background: #fff;
+	.goods-item {
+		display: flex;
+		flex-direction: column;
+		flex-flow: nowrap;
+		width: 100%;
+		padding-bottom: 40upx;
+		// &:nth-child(2n + 1) {
+		// 	margin-right: 4%;
+		// }
+	}
+	.image-wrapper {
+		width: 200upx;
+		height: 200upx;
+		border-radius: 3px;
+		overflow: visible;
+		image {
+			width: 200upx;
+			height: 200upx;
+			opacity: 1;
+			overflow: visible;
+		}
+	}
+	.goods-detail {
+		display: inline-block;
+		width: 100%;
+		padding: 0 30rpx;
+		.title {
+			font-size: $font-lg;
+			color: $font-color-dark;
+			line-height: 70upx;
+			width: 80%;
+			display: inline-block;
+		}
+		.subhead {
+			color: #333;
+			font-size: 30upx;
+		}
+		.price-box {
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			padding-right: 10upx;
+			font-size: 24upx;
+			color: $font-color-light;
+			.price {
+				.priceSale {
+					font-size: 40upx;
+					color: $uni-color-primary;
+					line-height: 1;
+					&:before {
+						content: '￥';
+						font-size: 26upx;
+					}	
+				}
+				.pricemart {
+					font-size: 25upx;
+					color: #000;
+					text-decoration:line-through;
+					line-height: 1;
+					&:before {
+						content: '￥';
+						font-size: 26upx;
+					}	
+				}
+			}
+			.goodsBtn {
+				font-size: 30upx;
+				color: #fff;
+				background: red;
+				height: 70upx;
+				line-height: 70upx;
+				width: 150upx;
+				padding: 0;
+				margin: 0;
+			}
+		}
+	}
+}
+.num {
+	display: flex;
+}
+.buy-type {
+	display: flex;
+	justify-content: flex-start;
+	label {
+		width: 50%;
+		text-align: center;
+	}
+}
+</style>
---
+++ cloudstore_uniapp_client/pages/agent/goods/agent/detail.vue
@@ -0,0 +1,665 @@
+<template>
+	<view class="container">
+		<nav-bar backState="1000" fontColor="#FFF"></nav-bar>
+		<view class="carousel">
+			<swiper indicator-dots circular="true" duration="400">
+				<swiper-item class="swiper-item" v-for="(item, index) in goods.goodsDetailPhotos" :key="index">
+					<view class="image-wrapper">
+						<image :src="item.url" class="loaded" mode="aspectFill"></image>
+					</view>
+				</swiper-item>
+			</swiper>
+		</view>
+ 
+		<view class="introduce-section">
+			<text class="title">{{ goods.goodsName }}</text>
+			<view class="price-box">
+				<text class="price-tip">价格:¥</text>
+				<text class="price">{{ goods.salePrice }}</text>
+				<text class="m-price">¥{{ goods.martPrice }}</text>
+			</view>
+			<!--
+			<view class="bot-row">
+				
+				<text>销量: {{ goods.sale }}</text>
+				<text>库存: {{ goods.stock }}</text>
+				<text>浏览量: {{ goods.hit }}</text>
+			
+			</view>
+			-->
+		</view>
+
+		<!--  分享 -->
+		<!-- <view class="share-section">
+			<view class="share-icon">
+				<text class="yticon icon-xingxing"></text>
+				返
+			</view>
+			<text class="tit">该商品分享可领49减10红包</text>
+			<text class="yticon icon-bangzhu1"></text>
+			<view class="share-btn">
+				立即分享
+				<text class="yticon icon-you"></text>
+			</view>
+		</view> -->
+
+
+
+
+		<view class="c-list">
+			<view class="c-row b-b">
+				<text class="tit">商品规格</text>
+			</view>
+			<view class="c-row b-b">
+					<view class="con-list">
+				<view class="table" v-if="goodsSku[0] != null">
+				 <view class="tr bg-w">
+				 <view class="th" v-for="  (item,index) in goodsSku[0].skuKeys " :key='index'>{{item}}</view>
+				 <view class="th">价格</view>
+				 </view>
+				  <block v-for='itm  in  goodsSku' :key='itm.id'>
+					   <view class="tr bg-g">
+					  <view class="td" v-for="  (item,index) in itm.skuValues " :key='index'>{{item}}</view>
+					  <view class="td">¥{{itm.price}}</view>
+					  </view>
+				  </block>
+				</view>
+				</view>
+				
+			</view>
+
+
+			<view class="c-row b-b">
+				<text class="tit">商品参数</text>
+			</view>
+			<view class="c-row b-b"   v-for='itm  in  goodsPropertyValue' :key='itm.id' v-if="itm.propertyType == 1 ">
+				<text class="tit">{{itm.goodsPropertyParamName}}</text>
+				<view class="con-list">
+					<text >¥{{itm.propertyValue}}
+					</text>
+				</view>
+			</view>
+			
+			<!--
+			<view class="c-row b-b">
+				<text class="tit">优惠券</text>
+				<text class="con t-r red">领取优惠券</text>
+				<text class="yticon icon-you"></text>
+			</view>
+             -->
+			 <view class="c-row b-b">
+			 	<text class="tit"></text>
+			 </view>
+			<view class="c-row b-b">
+				<text class="tit">产地</text>
+				<view class="con-list">
+					<text v-text="addressBygoods(goods)">
+					</text>
+				</view>
+			</view>
+
+			<view class="c-row b-b">
+				<text class="tit">活动内容</text>
+				<view class="con-list">
+					<text>
+						这里展示活动内容
+					</text>
+				</view>
+			</view>
+			<view class="c-row b-b">
+				<text class="tit">服务</text>
+				<view class="bz-list con">
+					<text>7天无理由退换货 ·</text>
+					<text>假一赔十 ·</text>
+				</view>
+			</view>
+		</view>
+
+		<!-- 评价 
+		<view class="eva-section">
+			<view class="e-header">
+				<text class="tit">评价</text>
+				<text>({{ consultCount.all }})</text>
+				<text class="tit">好评</text>
+				<text>({{ consultCount.goods }})</text>
+
+				<text class="tit">一般</text>
+				<text>({{ consultCount.general }})</text>
+
+				<text class="tit">差评</text>
+				<text>({{ consultCount.bad }})</text>
+				<text class="tip" v-if="consultCount.persent != 200">好评率 {{ consultCount.persent }}%</text>
+				<text class="yticon icon-you"></text>
+			</view>
+		</view>
+		-->
+
+
+		<view class="detail-desc">
+			<view class="d-header"><text>图文详情</text></view>
+			<view class="ricetext">
+				<rich-text :nodes="goodsMobileHtml"></rich-text>
+			</view>
+		</view>
+
+		<!-- 底部操作菜单 -->
+		<view class="page-bottom">
+			<navigator url="/pages/agent/goods/hotsale/hotsale" open-type="switchTab" class="p-b-btn">
+				<text class="yticon icon-xiatubiao--copy"></text>
+				<text>首页</text>
+			</navigator>
+			<navigator url="/pages/cart/cart" open-type="switchTab" class="p-b-btn">
+				<text class="yticon icon-gouwuche"></text>
+				<text>期待</text>
+			</navigator>
+			<view class="p-b-btn" :class="{ active: favorite }" @click="toFavorite(goods)">
+				<text class="yticon icon-shoucang"></text>
+				<text>收藏</text>
+			</view>
+
+			<view class="action-btn-group">
+				<button type="primary" class=" action-btn no-border buy-now-btn" @click="joinAgent">加入代理</button>
+				<button type="primary" class=" action-btn no-border add-cart-btn" @click="toAgent">我的小店</button>
+			</view>
+		</view>
+
+	</view>
+</template>
+
+<script>
+	import Api from '@/common/api';
+	import share from '@/components/share';
+	import navBar from '@/components/zhouWei-navBar';
+	import {
+		mapState
+	} from 'vuex';
+	let userInfo = uni.getStorageSync('userInfo');
+	export default {
+		components: {
+			share, navBar
+		},
+		data() {
+			return {
+
+				goods: {},
+				goodsSku: [],
+				goodsPropertyValue:[],
+				goodsMobileHtml:'',
+				favorite: true,
+			};
+		},
+		onShow() {
+			this.loadGoodHtml(this.goodsId);
+		},
+		   
+		// onShareAppMessage(res) {
+		// 	console.log(res)
+		// 	if (res.from === 'button') { // 来自页面内分享按钮
+		// 	}
+		// 	return {
+		// 		title: '商品',
+		// 		path: '/pages/goods/goodsDetail/goodsDetail?id=1'
+		// 	}
+		// },
+		async onLoad(ops) {
+			this.goodsId = ops.id;
+			if (this.goodsId) {
+				let params = {
+					goodsId: this.goodsId
+				};
+				let data = await Api.apiCall('post', Api.agent.goods.detail, params, false, false);
+				if (data) {
+					this.goods = data.result.goodsPicesBean;
+					this.goodsSku = data.result.goodsSku;
+					this.goodsPropertyValue =data.result.goodsPropertyValue;
+				}
+			}
+		},
+		filters: {
+		},
+
+		methods: {
+			
+			setmobileHtml(mobileHtml){
+				this.goodsMobileHtml = mobileHtml;
+			},
+			
+			loadGoodHtml(goodsId){
+				let params = {
+					goodsId: goodsId
+				};
+				let mobileHtml=this.setmobileHtml;
+				Api.apiCallbackCall('post', Api.agent.goods.loadHtml, params, false,false, function(data){
+					  mobileHtml(data.result.mobileHtml);
+				});
+			},
+			addressBygoods(goods) {
+				try {
+					return goods.provinceBean.name + " " + goods.cityBean.name + " " + goods.areaBean.name;
+				} catch (e) {
+					return "获取产地信息出错";
+				}
+			},
+			async joinAgent() {
+				let params = {
+					goodsId: this.goodsId,
+					activeId: 1
+				}
+				let data = await Api.apiCall('post', Api.agent.goods.save, params);
+				if (data) {
+					if (data.code === 0) {
+						uni.showToast({
+							title: '加入代理成功',
+							icon: 'none'
+						});
+						if (this.specClass == 'show') {
+							this.specClass = 'hide'
+						}
+					}
+					// console.log(data)
+				}
+			},
+			toAgent () {
+				uni.switchTab({
+				    url: '/pages/agent/home/index'
+				});
+			},
+			//收藏
+			toFavorite(item) {
+				if (userInfo && userInfo.id) {
+					this.favorite = !this.favorite;
+					let params = {
+						objId: item.id,
+						type: 1,
+						name: item.name,
+						meno1: item.pic,
+						meno2: item.price,
+						meno3: item.sale
+					};
+					Api.apiCall('post', Api.agent.goods.favoriteSave, params);
+
+				} else {
+					let url = '/pages/public/login';
+					uni.navigateTo({
+						url
+					});
+				}
+			},
+
+		}
+	};
+</script>
+
+<style lang="scss">
+	page {
+		background: $page-color-base;
+		padding-bottom: 160upx;
+	}
+
+	.icon-you {
+		font-size: $font-base + 2upx;
+		color: #888;
+	}
+
+	.carousel {
+		height: 722upx;
+		position: relative;
+
+		swiper {
+			height: 100%;
+		}
+
+		.image-wrapper {
+			width: 100%;
+			height: 100%;
+		}
+
+		.swiper-item {
+			display: flex;
+			justify-content: center;
+			align-content: center;
+			height: 750upx;
+			overflow: hidden;
+
+			image {
+				width: 100%;
+				height: 100%;
+			}
+		}
+	}
+
+	/* 标题简介 */
+	.introduce-section {
+		background: #fff;
+		padding: 20upx 30upx;
+
+		.title {
+			font-size: 32upx;
+			color: $font-color-dark;
+			height: 50upx;
+			line-height: 50upx;
+		}
+
+		.price-box {
+			display: flex;
+			align-items: baseline;
+			height: 64upx;
+			padding: 10upx 0;
+			font-size: 26upx;
+			color: $uni-color-primary;
+		}
+
+		.price {
+			font-size: $font-lg + 2upx;
+		}
+
+		.m-price {
+			margin: 0 12upx;
+			color: $font-color-light;
+			text-decoration: line-through;
+		}
+
+		.coupon-tip {
+			align-items: center;
+			padding: 4upx 10upx;
+			background: $uni-color-primary;
+			font-size: $font-sm;
+			color: #fff;
+			border-radius: 6upx;
+			line-height: 1;
+			transform: translateY(-4upx);
+		}
+
+		.bot-row {
+			display: flex;
+			align-items: center;
+			height: 50upx;
+			font-size: $font-sm;
+			color: $font-color-light;
+
+			text {
+				flex: 1;
+			}
+		}
+	}
+
+
+	.c-list {
+		font-size: $font-sm + 2upx;
+		color: $font-color-base;
+		background: #fff;
+
+		.c-row {
+			display: flex;
+			align-items: center;
+			padding: 20upx 15upx;
+			position: relative;
+		}
+
+		.tit {
+			width: 140upx;
+			float: right;
+		}
+
+		.con {
+			flex: 1;
+			color: $font-color-dark;
+
+			.selected-text {
+				margin-right: 10upx;
+			}
+		}
+
+		.bz-list {
+			height: 40upx;
+			font-size: $font-sm + 2upx;
+			color: $font-color-dark;
+
+			text {
+				display: inline-block;
+				margin-right: 30upx;
+			}
+		}
+
+		.con-list {
+			flex: 1;
+			display: flex;
+			flex-direction: column;
+			color: $font-color-dark;
+			line-height: 40upx;
+		}
+
+		.red {
+			color: $uni-color-primary;
+		}
+	}
+
+	/* 评价 */
+	.eva-section {
+		display: flex;
+		flex-direction: column;
+		padding: 20upx 30upx;
+		background: #fff;
+		margin-top: 16upx;
+
+		.e-header {
+			display: flex;
+			align-items: center;
+			height: 70upx;
+			font-size: $font-sm + 2upx;
+			color: $font-color-light;
+
+			.tit {
+				font-size: $font-base + 2upx;
+				color: $font-color-dark;
+				margin-right: 4upx;
+			}
+
+			.tip {
+				flex: 1;
+				text-align: right;
+			}
+
+			.icon-you {
+				margin-left: 10upx;
+			}
+		}
+	}
+
+	.eva-box {
+		display: flex;
+		padding: 20upx 0;
+
+		.portrait {
+			flex-shrink: 0;
+			width: 80upx;
+			height: 80upx;
+			border-radius: 100px;
+		}
+
+		.right {
+			flex: 1;
+			display: flex;
+			flex-direction: column;
+			font-size: $font-base;
+			color: $font-color-base;
+			padding-left: 26upx;
+
+			.con {
+				font-size: $font-base;
+				color: $font-color-dark;
+				padding: 20upx 0;
+			}
+
+			.bot {
+				display: flex;
+				justify-content: space-between;
+				font-size: $font-sm;
+				color: $font-color-light;
+			}
+		}
+	}
+
+
+
+	/*  详情 */
+	.detail-desc {
+		background: #fff;
+		//margin-top: 16upx;
+		width: 100%;
+		//margin:5px auto 20px;
+		text-align: justify;
+		font-size: 16px;
+
+		.d-header {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			height: 80upx;
+			font-size: $font-base + 2upx;
+			color: $font-color-dark;
+			position: relative;
+
+			text {
+				padding: 0 20upx;
+				background: #fff;
+				position: relative;
+				z-index: 1;
+			}
+
+			&:after {
+				position: absolute;
+				left: 50%;
+				top: 50%;
+				transform: translateX(-50%);
+				width: 300upx;
+				height: 0;
+				content: '';
+				border-bottom: 1px solid #ccc;
+			}
+		}
+
+		.ricetext {
+			width: 90%;
+			margin: 5px auto 20px;
+			text-align: justify;
+			font-size: 17px;
+		}
+	}
+
+
+
+
+
+
+	/* 底部操作菜单 */
+	.page-bottom {
+		position: fixed;
+		left: 30upx;
+		bottom: 30upx;
+		z-index: 95;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 690upx;
+		height: 100upx;
+		background: rgba(255, 255, 255, 0.9);
+		box-shadow: 0 0 20upx 0 rgba(0, 0, 0, 0.5);
+		border-radius: 16upx;
+
+		.p-b-btn {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+			font-size: $font-sm;
+			color: $font-color-base;
+			width: 96upx;
+			height: 80upx;
+
+			.yticon {
+				font-size: 40upx;
+				line-height: 48upx;
+				color: $font-color-light;
+			}
+
+			&.active,
+			&.active .yticon {
+				color: $uni-color-primary;
+			}
+
+			.icon-fenxiang2 {
+				font-size: 42upx;
+				transform: translateY(-2upx);
+			}
+
+			.icon-shoucang {
+				font-size: 46upx;
+			}
+		}
+
+		.action-btn-group {
+			display: flex;
+			height: 76upx;
+			border-radius: 100px;
+			overflow: hidden;
+			box-shadow: 0 20upx 40upx -16upx #fa436a;
+			box-shadow: 1px 2px 5px rgba(219, 63, 96, 0.4);
+			background: linear-gradient(to right, #ffac30, #fa436a, #f56c6c);
+			margin-left: 20upx;
+			position: relative;
+
+			&:after {
+				content: '';
+				position: absolute;
+				top: 50%;
+				right: 50%;
+				transform: translateY(-50%);
+				height: 28upx;
+				width: 0;
+				border-right: 1px solid rgba(255, 255, 255, 0.5);
+			}
+
+			.action-btn {
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				width: 180upx;
+				height: 100%;
+				font-size: $font-base;
+				padding: 0;
+				border-radius: 0;
+				background: transparent;
+			}
+		}
+	}
+
+   .table {
+    border: 0px solid darkgray;
+   }
+   .tr {
+    display: flex;
+    width: 100%;
+    height: 40px;
+   }
+   .td {
+    width:100%;
+    text-align: center;
+   }
+   .bg-w{
+    // background-color: #cff8fe;
+   }
+   .bg-g{
+    //background: #FFFFFF;
+	//border-bottom: 1px solid darkgray;
+   }
+   .th {
+    width: 100%;
+    justify-content: center;
+    background: #E6E6FA;
+    color: #000000;
+    display: flex;
+    align-items: center;
+   }
+
+</style>
