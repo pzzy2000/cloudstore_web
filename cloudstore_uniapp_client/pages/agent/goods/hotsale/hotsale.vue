bbfbde19ebacb7d7e42cca8ba793afc739fb58e8
Administrator <Administrator@192.168.0.107>
2020-07-05 17:31:55 +0800

    走购买流程

--- cloudstore_uniapp_client/pages/agent/goods/hotsale/hotsale.vue
+++ cloudstore_uniapp_client/pages/agent/goods/hotsale/hotsale.vue
@@ -280,10 +280,11 @@
 			//详情页
 			navToDetailPage(item) {
 				//测试数据没有写id，用title代替
-				let id = item.goodsId;
+				let goodsId = item.goodsId;
 				let activitId = item.activityId;
+				let agoodsid= item.id;
 				uni.navigateTo({
-					url: `/pages/agent/goods/agent/detail?id=${id}&activityId=${activitId}`
+					url: `/pages/agent/goods/agent/detail?goodsId=${goodsId}&activityId=${activitId}&agoodsid=${agoodsid}`
 				});
 			},
 
