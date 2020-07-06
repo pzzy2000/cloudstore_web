84c51d29aa83f3496590e2b62093a3ba9f640081
w-shang <13673772852@163.com>
2020-07-06 19:16:43 +0800

    '1'

--- cloudstore-admin/src/views/activity/addactivity.vue
+++ cloudstore-admin/src/views/activity/addactivity.vue
@@ -5,6 +5,7 @@
         <el-form-item label="活动名称：" prop="activityName">
           <el-input-dispatcher v-model="activityForm.activityName" placeholder="请输入活动名称" style="width: 350px" />
         </el-form-item>
+        <el-checkbox v-model="checked" style="margin-left: 120px">是否参加佣金活动</el-checkbox>
         <div style="text-align: right">
           <el-button type="primary" size="small" @click="subActname('activityForm')">提 交</el-button>
           <el-button size="small" @click="resetForm('activityForm')">重 置</el-button>
@@ -37,7 +38,8 @@
         rwDispatcherState: 'write',
         rules: {
           activityName: [{ required: true, message: '请输入活动名称', trigger: 'blur' }]
-        }
+        },
+        checked: true
       }
     },
     methods: {
@@ -47,15 +49,28 @@
       subActname(formName) {
         this.$refs[formName].validate((valid) => {
           if (valid) {
-            addActivity({
-              name: this.activityForm.activityName,
-              optType: "save"
-            }).then(res => {
-              if (res.result.code == 0) {
-                msg("添加活动成功");
-                this.$router.push("/sys/activity/list")
-              }
-            })
+            if(this.checked){
+              this.$confirm('勾选了参加佣金活动，是否继续?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+              }).then(() => {
+                this.submitAct();
+              }).catch(() => e)
+            }else{
+              this.submitAct();
+            }
+          }
+        })
+      },
+      submitAct() {
+        addActivity({
+          name: this.activityForm.activityName,
+          optType: "save"
+        }).then(res => {
+          if (res.result.code == 0) {
+            msg("添加活动成功");
+            this.$router.push("/sys/activity/list")
           }
         })
       },
