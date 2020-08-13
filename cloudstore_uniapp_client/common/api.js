e0e02f2e31faf7398376d7dded4b7182f747b5b3
win <win@win-PC>
2020-08-13 10:33:12 +0800

    i

--- cloudstore_uniapp_client/common/api.js
+++ cloudstore_uniapp_client/common/api.js
@@ -1,7 +1,7 @@
 import store from '../store/index';
 export default {
 	BASEURI: 'http://106.52.184.24:18888/platform/',
-	// BASEURI: 'https://api.sz-guochuang.com/platform/',
+	//BASEURI: 'https://api.sz-guochuang.com/platform/',
 	
 	h5Appid: 'wxb4660f37187c0b8e', // h5微信登录的appId  暂时测试用
 	
